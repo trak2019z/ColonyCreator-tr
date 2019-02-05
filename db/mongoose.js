cb59ba91a80897261d4a89dbbcd928b339101376
Grzegorz <osowskigrzegorz95@gmail.com>
2019-02-05 21:51:35 +0100

    Wrzucenie linku do bazy

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/Colony', {
+mongoose.connect('mongodb+srv://admin:admin@cluster0-rqy2g.mongodb.net/test?retryWrites=true', {
     useNewUrlParser: true
 })
     .then(() => console.log('Connected..'))
